266b737cfd6fbe060c3e77adb534cfc3c21ca62b
julian <34633646+julianbejarano@users.noreply.github.com>
2026-05-30 20:36:54 -0500

    Add support information and developer credits

--- extraer-actas.py
+++ extraer-actas.py
@@ -428,7 +428,14 @@ def generar_texto_informe(carpetas_actas: dict) -> str:
     separador_fino   = "-" * 40
 
     lineas = [
+        "Herramienta de apoyo al diligenciamiento de informes",
+        "Esta aplicación puede contener fallos, no es oficial de ninguna entidad",
+        "Desarrollador : Julian Bejarano",
+        "Si deseas contribuir al desarrollador puedes invitarle un café",
+        "O aportarle 10 lks por Nequi al 3194796728",
+        "Con tus aportes se puede mejorar la aplicación y mantener su buen funcionamiento",
         "RESUMEN DE ACTAS",
+        "VERIFICAR LA INFORMACIÓN ES FUNDAMENTAL"         
         f"Generado el: {datetime.today().strftime('%d-%m-%Y %H:%M')}",
         separador_grueso,
     ]
